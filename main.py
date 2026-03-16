a8b2e1045af4208b6feccdc3799f0f3f79f2af76
Funsho <oluwafunshoalabi97@gmail.com>
2026-03-16 18:28:09 +0100

    #

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from strategy import analyze, symbols
 from fastapi.middleware.cors import CORSMiddleware
 import time
 from datetime import datetime, timezone
+import os
+import uvicorn
+
+port = int(os.environ.get("PORT", 8000))
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 app = FastAPI()
 
