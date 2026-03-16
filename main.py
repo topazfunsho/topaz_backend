743aceacf41ea17f7a040c461eff5fa82a0e33c4
Funsho <oluwafunshoalabi97@gmail.com>
2026-03-16 19:29:40 +0100

    #

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime, timezone
 import os
 import uvicorn
 
-port = int(os.environ.get("PORT", 8000))
+port = int(os.environ.get("PORT", 10000))
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=port)
