2e613c9fc733f120acc1ae1abe61e320457ad4b1
Funsho <oluwafunshoalabi97@gmail.com>
2026-03-16 20:29:18 +0100

    #

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ from datetime import datetime, timezone
 import os
 import uvicorn
 
-port = int(os.environ.get("PORT", 10000))
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
-
 app = FastAPI()
 
 origins = [
@@ -25,6 +20,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+port = int(os.environ.get("PORT", 10000))
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 bot_running = False
 
